b74593e4cba0a5d68711c37bf019e294a9e7f44b
matikitli <m.kitlinos@gmail.com>
2018-02-05 12:53:07 +0000

    Simple scrolling added(jquery easing)

---
+++ js/mainJs.js
@@ -0,0 +1,23 @@
+(function($) {
+
+    // DELAY SCROLLING WITH JQUERY-EASING
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+
+    // HIGHLIGHT ACTUAL POSITION
+    $('body').scrollspy({
+        target: '#navbar-vertical'
+    });
+
+})(jQuery);
